69650ee0211d50f2cd0b2bcec0651476a0c837f1
Emily Shenfield <emily.shenfield@gmail.com>
2015-08-05 20:32:08 -0700

    feat(new player): added new player modal with styling

--- client/components/newPlayerModal/newPlayer.ctrl.js
+++ client/components/newPlayerModal/newPlayer.ctrl.js
@@ -1,51 +1,54 @@
-angular
-  .module('foosballFrenzy')
-  .controller('NewPlayerCtrl', function($scope, $modal, Players) {
+(function() {
+  angular
+    .module('foosballFrenzy')
+    .controller('NewPlayerCtrl', function($scope, $state, $modal, Players) {
 
-    $scope.data = {};
+      $scope.data = {};
 
-    Players.getAllPlayers()
-    .then(function(players) {
-      $scope.data.players = players;
-    });
+      Players.getAllPlayers()
+      .then(function(players) {
+        $scope.data.players = players;
+      });
 
-    $scope.open = function(size) {
-      var modalInstance = $modal.open({
-        animation: true,
-        templateUrl: 'components/newPlayerModal/newPlayer.html',
-        controller: 'NewPlayerInstanceCtrl',
-        size: size,
-        resolve: {
-          players: function () {
-            return $scope.data.players;
+      $scope.open = function(size) {
+        var modalInstance = $modal.open({
+          animation: true,
+          templateUrl: 'components/newPlayerModal/newPlayer.html',
+          controller: 'NewPlayerInstanceCtrl',
+          size: size,
+          resolve: {
+            players: function () {
+              return $scope.data.players;
+            }
           }
-        }
-      });
-
-      modalInstance.result.then(function(name) {
-        Players.createPlayer(name)
-        .then(function(newPlayer) {
-          console.log('created new player!', newPlayer);
         });
-      });
-    };
-});
 
-angular
-  .module('foosballFrenzy')
-  .controller('NewPlayerInstanceCtrl', function($scope, $modalInstance, players) {
-    $scope.players = players;
+        modalInstance.result.then(function(name) {
+          Players.createPlayer(name)
+          .then(function(newPlayer) {
+            console.log('created new player!', newPlayer);
+            $state.reload();
+          });
+        });
+      };
+  });
 
-    $scope.ok = function() {
-      // check if name exists already in players
-      // if it does, 
-        // tell user
-      // else 
-      var name = $scope.name
-      $modalInstance.close(name);
-    };
+  angular
+    .module('foosballFrenzy')
+    .controller('NewPlayerInstanceCtrl', function($scope, $modalInstance, players) {
+      $scope.players = players;
 
-    $scope.cancel = function() {
-      $modalInstance.dismiss('cancel');
-    };
-});
+      $scope.ok = function() {
+        // check if name exists already in players
+        // if it does, 
+          // tell user
+        // else 
+        var name = $scope.name
+        $modalInstance.close(name);
+      };
+
+      $scope.cancel = function() {
+        $modalInstance.dismiss('cancel');
+      };
+  });
+})();
