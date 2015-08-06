3fd958baeae3e26995ed0d4183baa1fad3e28da6
Emily Shenfield <emily.shenfield@gmail.com>
2015-08-06 10:52:58 -0700

    dev(graph): added d3 graph elements, labels, and styling

--- client/app/directives.js
+++ client/app/directives.js
@@ -1,75 +1,75 @@
 (function() {
   angular
     .module('foosballFrenzy')
-    .directive('graphChart', function(d3Service, Players) {
+    .directive('graphChart', function(d3Service, Players, Matches) {
       return {
         restrict: 'EA',
         scope: {},
         link: function(scope, element, attrs) {
           Players.getAllPlayers()
             .then(function(players) {
-              var graph = {
-                nodes: players,
-                links: [
-                  // {"source":1, "target":0, "value":6},
-                  // {"source":1, "target":3, "value":2},
-                  // {"source":1, "target":5, "value":0},
-                  // {"source":2, "target":3, "value":1},
-                  // {"source":3, "target":4, "value":2},
-                  // {"source":3, "target":0, "value":1},
-                  // {"source":4, "target":6, "value":0}
-                ]
-              };
+              Matches.getLinkMap()
+              .then(function(links) {
+                var graph = {
+                  nodes: players,
+                  links: links
+                };
 
-              d3 = d3Service;
-              var width = 960,
-                  height = 500;
+                d3 = d3Service;
+                var width = 960,
+                    height = 500;
 
-              var color = d3.scale.category20();
+                var color = d3.scale.category20();
 
-              var force = d3.layout.force()
-                  .charge(-120)
-                  .linkDistance(30)
-                  .size([width, height]);
+                var force = d3.layout.force()
+                    .charge(-120)
+                    .linkDistance(100)
+                    .size([width, height]);
 
-              var svg = d3.select(element[0]).append("svg")
-                  .attr("width", width)
-                  .attr("height", height);
+                var svg = d3.select(element[0]).append("svg")
+                    .attr("width", width)
+                    .attr("height", height);
 
-                force
-                    .nodes(graph.nodes)
-                    .links(graph.links)
-                    .start();
+                  force
+                      .nodes(graph.nodes)
+                      .links(graph.links)
+                      .start();
 
-                var link = svg.selectAll(".link")
-                    .data(graph.links)
-                    .enter().append("line")
-                    .attr("class", "link")
-                    .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+                  var link = svg.selectAll(".link")
+                      .data(graph.links)
+                      .enter().append("line")
+                      .attr("class", "link")
+                      .style("stroke-width", function(d) { return d.value });
 
-                var node = svg.selectAll(".node")
-                    .data(graph.nodes)
-                  .enter().append("circle")
-                    .attr("class", "node")
-                    .attr("r", function(d) {return Math.pow(d.rating, 1/4)})
-                    .style("fill", function(d) { return color(2); })
-                    .call(force.drag);
+                  var node = svg.selectAll(".node")
+                      .data(graph.nodes)
+                      .enter().append("g")
+                      .attr("class", "node")
+                      .call(force.drag);
 
-                    node.append("text")
-                        .attr("dx", 12)
-                        .attr("dy", ".35em")
-                        .text(function(d) { return d.name });
+                      node.append("circle")
+                          .attr("r", function(d) { return Math.pow(d.rating, 1/3); })
+                          .style("fill", function(d) { return color(d.rating); })
+                          .attr("x", -8)
+                          .attr("y", -8)
+                          .attr("width", 16)
+                          .attr("height", 16);
 
-                force.on("tick", function() {
-                  link.attr("x1", function(d) { return d.source.x; })
-                      .attr("y1", function(d) { return d.source.y; })
-                      .attr("x2", function(d) { return d.target.x; })
-                      .attr("y2", function(d) { return d.target.y; });
+                      node.append("text")
+                          .attr("dx", 12)
+                          .attr("dy", ".35em")
+                          .text(function(d) { return d.name });
 
-                  node.attr("cx", function(d) { return d.x; })
-                      .attr("cy", function(d) { return d.y; });
-                });
-            })
+                  force.on("tick", function() {
+                    link.attr("x1", function(d) { return d.source.x; })
+                        .attr("y1", function(d) { return d.source.y; })
+                        .attr("x2", function(d) { return d.target.x; })
+                        .attr("y2", function(d) { return d.target.y; });
+
+                   node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+                  });
+              });
+            });
         }
       };
     });
