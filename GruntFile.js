87fe96b0e3aacdb40b3b1266ba6b865d76949925
Emily Shenfield <emily.shenfield@gmail.com>
2015-08-06 11:45:04 -0700

    chore(grunt): created gruntfile and added dependencies, replaced script tags with built script in index

---
+++ GruntFile.js
@@ -0,0 +1,87 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Building
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['./client/app/**/*.js', './client/components/**/*.js'],
+        dest: './client/built/<%= pkg.name %>.js'
+      }
+    },
+
+    uglify: {
+      dist: {
+        files: {
+          './client/built/<%= pkg.name %>.min.js' : ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+
+    // Watching
+    watch: {
+      scripts: {
+        files: [
+          'client/app/**/*.js',
+          'client/components/**/*.js'
+        ],
+        tasks: [
+          'concat',
+          'uglify'
+        ]
+      }
+    },
+
+    nodemon: {
+      dev: {
+        script: 'server/server.js'
+      },
+      ignore: ['node_modules/**', 'client/bower_components/**'],
+      options: {
+        'no-preload': true
+      }
+    }
+  });
+
+  // Loads all grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  ////////////////////////////////////////////////////
+  // Primary grunt tasks
+  ////////////////////////////////////////////////////
+
+  grunt.registerTask('serve', function (target) {
+
+    grunt.task.run([ 'build' ]);
+
+    var nodemon = grunt.util.spawn({
+         cmd: 'grunt',
+         grunt: true,
+         args: 'nodemon'
+    });
+    nodemon.stdout.pipe(process.stdout);
+    nodemon.stderr.pipe(process.stderr);
+
+    grunt.task.run([ 'watch' ]);
+
+  });
+
+  ////////////////////////////////////////////////////
+  // Secondary grunt tasks
+  ////////////////////////////////////////////////////
+
+  grunt.registerTask('build', [
+    'concat',
+    'uglify'
+  ]);
+
+  grunt.registerTask('build-deploy', [
+    'concat',
+    'uglify'
+  ]);
+};
