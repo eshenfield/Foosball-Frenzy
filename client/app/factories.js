5eef9af1a8ecb044c75dc459ff0cc08ac66cc0df
Emily Shenfield <emily.shenfield@gmail.com>
2015-08-05 20:28:18 -0700

    dev(factory): added first function to Matches factory

--- client/app/factories.js
+++ client/app/factories.js
@@ -1,49 +1,71 @@
-angular
-  .module('foosballFrenzy')
-  .factory('Players', function($http) {
-    var getAllPlayers = function() {
-      return $http({
-        method: 'GET',
-        url: '/api/players'
-      })
-      .then(function(res) {
-        return res.data;
-      })
-      .catch(function(err) {
-        console.error('Error!', err);
-      });
-    };
+(function() {
+  angular
+    .module('foosballFrenzy')
+    .factory('Players', function($http) {
+      var getAllPlayers = function() {
+        return $http({
+          method: 'GET',
+          url: '/api/players'
+        })
+        .then(function(res) {
+          return res.data;
+        })
+        .catch(function(err) {
+          console.error('Error!', err);
+        });
+      };
 
-    var getPlayer = function(name) {
-      return $http({
-        method: 'GET',
-        url: '/api/players/' + name 
-      })
-      .then(function(res) {
-        return res.data;
-      })
-      .catch(function(err) {
-        console.error('Error! ', err);
-      });
-    };
+      var getPlayer = function(name) {
+        return $http({
+          method: 'GET',
+          url: '/api/players/' + name 
+        })
+        .then(function(res) {
+          return res.data;
+        })
+        .catch(function(err) {
+          console.error('Error! ', err);
+        });
+      };
 
-    var createPlayer = function(name) {
-      return $http({
-        method: 'POST',
-        url: 'api/players',
-        data: {'name': name}
-      })
-      .then(function(res) {
-        return res.data;
-      })
-      .catch(function(err) {
-        console.error('Error! ', err);
-      });
-    };
+      var createPlayer = function(name) {
+        return $http({
+          method: 'POST',
+          url: 'api/players',
+          data: {'name': name}
+        })
+        .then(function(res) {
+          return res.data;
+        })
+        .catch(function(err) {
+          console.error('Error! ', err);
+        });
+      };
 
-    return {
-      getAllPlayers: getAllPlayers,
-      getPlayer: getPlayer,
-      createPlayer: createPlayer
-    };
-  })
+      return {
+        getAllPlayers: getAllPlayers,
+        getPlayer: getPlayer,
+        createPlayer: createPlayer
+      };
+    })
+    .factory('Matches', function($http) {
+      var createMatch = function(winner, loser, date, newWinningRating, newLosingRating) {
+        date = new Date(date);
+        return $http({
+          method: 'POST',
+          url: 'api/matches',
+          data: {'winner': winner.name, 'loser': loser.name, 'date': date, 'newWinningRating': newWinningRating, 'newLosingRating': newLosingRating}
+        })
+        .then(function(res) {
+          return res.data;
+        })
+        .catch(function(err) {
+          console.error('Error! ', err);
+        });
+      };
+
+      return {
+        createMatch: createMatch
+      }
+    });
+})();
